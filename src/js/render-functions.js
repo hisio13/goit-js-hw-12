406f4e3c9df70e16ea61654f74772239857f0966
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-02-24 20:42:36 +0100

    init

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,5 @@
-
 import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: 'alt',
